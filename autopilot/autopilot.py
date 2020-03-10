3816cb94b79e5daedc9673451f0f2b81b90716ec
amoidimi <58940356+amoidimi@users.noreply.github.com>
2020-03-10 13:55:07 +0000

    Update autopilot.py

--- autopilot/autopilot.py
+++ autopilot/autopilot.py
@@ -63,7 +63,18 @@ class AutoPilot:
         """
         while not self._terminate:
             ret, frame = self.camera.read()
+            
 
+         def img_preprocess(image):
+             height, _, _ = image.shape
+             image = image[int(height*0.15):,:,:]  # remove top half of the image, as it is not relavant for lane following
+             image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)  # Nvidia model said it is best to use YUV color space
+             image = cv2.GaussianBlur(image, (3,3), 0)
+             image = cv2.resize(image, (200,120)) # input image size (200,66) Nvidia model
+             image = image / 255 # normalizing, the processed image becomes black for some reason.  do we need this?
+             return image
+          
+         frame = img_preprocess(frame)
             # !! Use machine learning to determine angle and speed (if necessary - you may decide to use fixed speed) !!
 
             angle = model.predict(frame)
